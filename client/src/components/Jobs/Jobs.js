47d7d26586fae0ba4961b5a90ecabb5b25b13c61
botr99 <brandonong999@gmail.com>
2021-06-07 18:31:36 +0800

    Add "Jobs per Page" functionality
    By default, 10 jobs will be shown per page.
    Options of 5, 10 and 15 jobs per page are presented.
    When the options are changed, the user gets directed back to the
    first page with the corresponding jobs per page being shown.

--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -3,63 +3,101 @@ import JobsApi from "../../apis/JobsApi";
 import { JobsContext } from "../../context/JobsContext";
 import { Link } from "react-router-dom";
 import Job from "./Job/Job";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
 import Pagination from "@material-ui/lab/Pagination";
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
 const Jobs = () => {
+  const classes = useStyles();
   const { jobs, setJobs } = useContext(JobsContext);
   const [page, setPage] = useState(1);
   const [pageCount, setPageCount] = useState(1);
+  const [limit, setLimit] = useState(10);
 
-  const handleChange = (e, value) => {
+  const handlePageChange = (e, value) => {
     setPage(value);
+  };
+
+  const handleLimitChange = (e) => {
+    setPage(1); // go back to the front page
+    setLimit(e.target.value); // change the number of jobs per page
   };
 
   useEffect(() => {
     const fetchJobs = async () => {
       try {
-        const res = await JobsApi.get(`/?page=${page}`);
+        const res = await JobsApi.get(`/?page=${page}&limit=${limit}`);
         setJobs(res.data.data);
         setPage(res.data.page);
         setPageCount(res.data.pageCount);
+        setLimit(res.data.limit);
       } catch (err) {
         console.log(err);
       }
     };
 
     fetchJobs();
-  }, [page]);
+  }, [page, limit]);
 
   return (
     <>
-      <h1>Available Jobs</h1>
-      <Link to="/jobs/new">
-        <button type="button">Add Job</button>
-      </Link>
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <h1>Available Jobs</h1>
+        <Link to="/jobs/new">
+          <button type="button">Add Job</button>
+        </Link>
+      </Box>
+
+      {/* <SearchBar />
+      Category/Date filters */}
 
       {jobs && (
-        <div>
-          <Pagination
-            count={pageCount}
-            page={page}
-            variant="outlined"
-            color="primary"
-            showFirstButton
-            showLastButton
-            onChange={handleChange}
-          />
+        <Box>
           {jobs.map((job) => (
             <Job key={job._id} job={job} />
           ))}
-          <Pagination
-            count={pageCount}
-            page={page}
-            variant="outlined"
-            color="primary"
-            showFirstButton
-            showLastButton
-            onChange={handleChange}
-          />
-        </div>
+          <Box
+            display="flex"
+            justifyContent="space-between"
+            alignItems="center"
+            flexWrap="wrap"
+          >
+            <Pagination
+              count={pageCount}
+              page={page}
+              variant="outlined"
+              color="primary"
+              showFirstButton
+              showLastButton
+              onChange={handlePageChange}
+            />
+            <FormControl className={classes.formControl}>
+              <InputLabel>Jobs per Page</InputLabel>
+              <Select
+                labelId="limit-select-label"
+                value={limit}
+                onChange={handleLimitChange}
+              >
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={10}>10</MenuItem>
+                <MenuItem value={15}>15</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
+        </Box>
       )}
     </>
   );
