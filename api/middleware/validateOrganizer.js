de055d8653096c062bd8c3ba4d619c9b728eded4
botr99 <brandonong999@gmail.com>
2021-06-24 17:00:42 +0800

    Add more validation middleware

---
+++ api/middleware/validateOrganizer.js
@@ -0,0 +1,13 @@
+import ROLES from "../utils/roles.js";
+
+const validateOrganizer = (req, res, next) => {
+  if (
+    req.user?.role === ROLES.Admin ||
+    req.user?.name === req.jobDetail.organizer
+  ) {
+    return next();
+  }
+  res.status(401).json({ message: "Unauthorized access" });
+};
+
+export default validateOrganizer;
