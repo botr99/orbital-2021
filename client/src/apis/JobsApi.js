015e202c8c639ee2cd4f47e10eff34e99c187dcf
botr99 <brandonong999@gmail.com>
2021-06-29 20:41:25 +0800

    Set up react-query and abstract out jobs api calls

--- client/src/apis/JobsApi.js
+++ client/src/apis/JobsApi.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 import url from "../config/url";
 
-const API = axios.create({
+const JobsApi = axios.create({
   baseURL: `${url}/api/jobs`,
 });
 
-API.interceptors.request.use((req) => {
+JobsApi.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     req.headers.authorization = `Bearer ${
       JSON.parse(localStorage.getItem("profile")).token
@@ -15,4 +15,47 @@ API.interceptors.request.use((req) => {
   return req;
 });
 
-export default API;
+export const getCategories = async () => {
+  const { data } = await JobsApi.get("/categories");
+  return data;
+};
+
+export const getApprovedJobs = async () => {
+  const { data } = await JobsApi.get("/");
+  return data;
+};
+
+export const getUnapprovedJobs = async () => {
+  const { data } = await JobsApi.get("/unapproved");
+  return data;
+};
+
+export const getJobRegistrations = async (jobId) => {
+  const { data } = await JobsApi.get(`/${jobId}/registrations`);
+  return data;
+};
+
+export const getJobDetail = async (jobId) => {
+  const { data } = await JobsApi.get(`/${jobId}`);
+  return data;
+};
+
+export const postJobRegistration = async (jobId) => {
+  const { data } = await JobsApi.post(`/${jobId}/registrations`);
+  return data;
+};
+
+export const postJob = async () => {
+  const { data } = await JobsApi.post("/");
+  return data;
+};
+
+export const updateJob = async (jobId) => {
+  const { data } = await JobsApi.patch(`/${jobId}`);
+  return data;
+};
+
+export const deleteJob = async (jobId) => {
+  const { data } = await JobsApi.delete(`/${jobId}`);
+  return data;
+};
--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
-import authReducer from './reducers/auth';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import authReducer from "./reducers/auth";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const store = createStore(authReducer, compose(applyMiddleware(thunk)));
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
