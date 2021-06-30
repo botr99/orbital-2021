3213e5c863fa0d813615b07ebaf0ef470b8b5d71
botr99 <brandonong999@gmail.com>
2021-07-01 02:31:25 +0800

    Modify the fetching data and search function of the Job component
    - Refactor Job to use react-query
    - Pressing search button is now the only way to trigger both
    search and/or filters at once

--- client/src/components/CategoryFilter.js
+++ client/src/components/CategoryFilter.js
@@ -1,42 +1,36 @@
-import React, { useContext } from "react";
-import { JobsCategoryContext } from "../context/JobsCategoryContext";
+import { useQuery } from "react-query";
+import { getCategories } from "../apis/JobsApi";
 
-const CategoryFilter = ({
-  filteredCategories,
-  setFilteredCategories,
-  setPage,
-}) => {
-  const categories = useContext(JobsCategoryContext);
+const CategoryFilter = ({ selectedCategories, setSelectedCategories }) => {
+  const { data: categories } = useQuery("categories", getCategories);
 
   const handleChange = (e) => {
     const isChecked = e.target.checked;
 
     if (isChecked) {
-      setFilteredCategories([...filteredCategories, e.target.value]);
+      setSelectedCategories([...selectedCategories, e.target.value]);
     } else {
-      setFilteredCategories(
-        filteredCategories.filter((elem) => elem !== e.target.value)
+      setSelectedCategories(
+        selectedCategories.filter((elem) => elem !== e.target.value)
       );
     }
-    setPage(1);
   };
 
   return (
     <>
-      {categories.length > 0 &&
-        categories.map((category) => (
-          <div className="form-check form-check-inline" key={category}>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value={category}
-              onChange={handleChange}
-            />
-            <label className="form-check-label" htmlFor="inlineCheckbox1">
-              {category}
-            </label>
-          </div>
-        ))}
+      {categories?.map((category) => (
+        <div className="form-check form-check-inline" key={category}>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value={category}
+            onChange={handleChange}
+          />
+          <label className="form-check-label" htmlFor="inlineCheckbox1">
+            {category}
+          </label>
+        </div>
+      ))}
     </>
   );
 };
