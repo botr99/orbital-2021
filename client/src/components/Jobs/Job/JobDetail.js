08274100fb283817fc49edabca876d211bc12e6f
felix-ong <felixongweicong@gmail.com>
2021-07-11 19:33:18 +0800

    Change link in JobDetail to correct profile route

--- client/src/components/Jobs/Job/JobDetail.js
+++ client/src/components/Jobs/Job/JobDetail.js
@@ -106,7 +106,7 @@ const JobDetail = () => {
                     by{" "}
                     <Link
                       className={classes.link}
-                      to={`/${jobDetail.organizer}`}>
+                      to={`/organizers/${jobDetail.organizer}`}>
                       {jobDetail.organizer}
                     </Link>
                   </Typography>
